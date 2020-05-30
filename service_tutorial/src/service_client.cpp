4f830dc6c40322b9df8a0f0148d38bfd57298302
Ramune6110 <arduino6110@gmail.com>
2020-05-30 18:31:11 +0900

    [Fix]Cmake

--- service_tutorial/src/service_client.cpp
+++ service_tutorial/src/service_client.cpp
@@ -2,26 +2,26 @@
 #include "service_tutorial/Srvtutorial.h"
 #include <cstdlib>
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "service_client");
 
     if (argc != 3) {
-        ROS_INFO("Error!!!!");
+        ROS_INFO("Error!!!");
         return 1;
     }
 
     ros::NodeHandle nh;
-    ros::ServiceClient client = nh.serviceClient<service_tutorial::Srvtutorial>("add_two_int");
+    ros::ServiceClient client = nh.serviceClient<service_tutorial::Srvtutorial>("ros_srv");
 
     service_tutorial::Srvtutorial srv;
     srv.request.a = atoll(argv[1]);
     srv.request.b = atoll(argv[2]);
 
     if (client.call(srv)) {
-        ROS_INFO((long int)srv.response.result);
+        ROS_INFO("%ld", (long int)srv.response.result);
     } else {
-        ROS_INFO("Error!!!!");
+        ROS_INFO("Error!!");
         return 1;
     }
 
